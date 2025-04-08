7d7c484a96db361d6fe2e6ecf5cedba2fa7ba857
WhyAreUsernames <thnea001@osloskolen.no>
2025-04-08 18:00:34 +0200

    firstFrile.py

--- firstFrile.py
+++ firstFrile.py
@@ -2,3 +2,8 @@ x = 10
 for i in range(0, x):
   x = int(input("please type a number less than or equal to the one printed above, if you want this to end"))
   print(x)
+
+
+#blah blah
+
+  #some stuff for setsint githun
